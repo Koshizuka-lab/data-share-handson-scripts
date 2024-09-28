49cb85e12ef7e1bea0ce96be83b4f93734f9f99b
someone <someone@example.jp>
2024-09-28 01:59:08 +0000

    data-share-handson

---
+++ set-containers/7-all_set.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cd ${WORKDIR}/data-share-handson-scripts/set-containers
+bash 1-ckan_set.sh
+
+cd ${WORKDIR}/data-share-handson-scripts/set-containers
+bash 2-prov_set.sh
+
+cd ${WORKDIR}/klab-connector-v4/misc/authorization
+./image_build_fastapi.sh
+./image_build_keycloak.sh
+docker image ls | grep fastapi
+docker image ls | grep keycloak
+
+cd ${WORKDIR}/klab-connector-v4/misc/authorization
+./image_build_fastapi.sh
+./image_build_keycloak.sh
+docker image ls | grep fastapi
+docker image ls | grep keycloak
+
+cd ${WORKDIR}/data-share-handson-scripts/set-containers
+bash 3-authz_set.sh
+
+cd ${WORKDIR}/data-share-handson-scripts/set-containers
+bash 4-cons_set.sh
+
+cd ${WORKDIR}/data-share-handson-scripts/set-containers
+bash 5-webapp_set.sh
+
+cd ${WORKDIR}/data-share-handson-scripts/set-containers
+bash 6-http-server_set.sh
